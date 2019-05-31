fe610186ab26e967a0fc6648d8c36effd30e4c34
alivenyung <alivenyung@gmail.com>
2019-05-31 22:41:23 +0800

    延遲時間修改

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(function(){
 	$("#Logo01").fadeIn(1000);
-	$("#Load").delay(3000).fadeOut(1000).queue(function (next) { 
+	$("#Load").delay(4000).fadeOut(1000).queue(function (next) { 
 		$("#Slogan > :nth-child(1)").show();
 		$("#Slogan > :nth-child(1)").addClass("SloganAnimate").delay(2500).queue(function (next){
 			$("#Slogan > :nth-child(2)").show();
