f3a624852b5ec1d399fc599631ebf65ff13ea475
alivenyung <alivenyung@gmail.com>
2019-05-31 13:44:38 +0800

    Create&首頁

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function(){
+	$("#Logo01").fadeIn(1000);
+	$("#Load").delay(1500).fadeOut(1000).queue(function (next) { 
+		$("#Slogan > :nth-child(1)").show();
+		$("#Slogan > :nth-child(1)").addClass("SloganAnimate").delay(2500).queue(function (next){
+			$("#Slogan > :nth-child(2)").show();
+			$("#Slogan > :nth-child(2)").addClass("SloganAnimate")
+			next();
+		});
+		next();
+	});
+
+});
